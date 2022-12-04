e5fd98187e7a7a534a32f540bd86a40814f76474
Joshua Roland <joblaze4real@gmail.com>
2022-12-03 16:06:56 -0800

    CPython - Python lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of ints"""
-    for row in matrix:
-        for i in row:
-            if i != row[-1]:
-                print("{:d}".format(i), end="")
-            else:
-                print("{:d}".format(i), end="")
+    for x in matrix:
+        for y in x:
+            print('{:d}'.format(y), end='')
+            if y != x[-1]:
+                print(end=' ')
         print()
