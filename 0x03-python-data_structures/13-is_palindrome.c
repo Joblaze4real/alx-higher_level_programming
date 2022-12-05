ce2c1c08705a4da7a7fbbb8a76e8ae5e2a08b38d
Joshua Roland <joblaze4real@gmail.com>
2022-12-05 07:29:00 -0800

    Hope it works now - Linked List Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,56 +1,38 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * list_len - finds no. of elements ina linked list.
- * @h: pointer to linked list.
+ * checkpal - function to check if list is a palindrome
+ * @start: start position of list
+ * @end: end position of list
  *
- * Return: number of elements in linked list.
+ * Return: 1 if list is a palindrome, 0 if not.
  */
-size_t list_len(listint_t *h)
-{
-	size_t  nodes = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+int checkpal(listint_t **start, listint_t *end)
+{
+	if (end == NULL)
+		return (1);
+
+	if (checkpal(start, end->next) == 1 && (*start)->n == end->n)
 	{
-		nodes++;
-		h = h->next;
+		*start = (*start)->next;
+		return (1);
 	}
-	return (nodes);
+
+	return (0);
 }
 
+
 /**
-* is_palindrome - checks if a singly linked list is a palindrome.
-* @head: double pointer to head of d-list.
-*
-* Return: 1 if palindrome, 0 if not.
-*/
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: list to check
+ *
+ * Return: 1 if list is a palindrome, 0 if not.
+ */
+
 int is_palindrome(listint_t **head)
 {
-	int *nArr, i = 0, j = 0, len = 0;
-	listint_t *temp;
-
-	if (*head == NULL)
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
 		return (1);
-	temp = *head;
-	len = list_len(temp);
-	nArr = (int *)malloc(sizeof(int) * len);
-	if (nArr == NULL)
-		return (2);
-	temp = *head;
-	while (temp != NULL)
-	{
-		nArr[j] = temp->n;
-		j++;
-		temp = temp->next;
-	}
-	for (i = 0, j = len - 1; i < j; i++, j--)
-	{
-		if (nArr[i] != nArr[j])
-			return (0);
-	}
-	return (1);
+
+	return (checkpal(head, *head));
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,8 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_H_INCLUDED
+#define LISTS_H_INCLUDED
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
@@ -23,5 +24,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int checkpal(listint_t **start, listint_t *end);
 
 #endif /* LISTS_H */
