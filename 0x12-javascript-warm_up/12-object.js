838e6f9565a9610f984e128555f2b87741e3605e
Joshua Roland <joblaze4real@gmail.com>
2023-03-15 15:27:00 +0000

    Javascript is amazing!

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Updated the value 12 in this script
+ * to 89
+ */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
